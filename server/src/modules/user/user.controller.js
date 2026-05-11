b53abc0331942d224edf0ae02fc91c7d263d7662
Harsh091234 <hsharma4090@gmail.com>
2026-05-11 19:35:00 +0530

    ui and bugs fixes

--- server/src/modules/user/user.controller.js
+++ server/src/modules/user/user.controller.js
@@ -1,10 +1,18 @@
+import redis from "../../../config/redis.js";
 import User from "./user.model.js";
 
+const root = "demo_pract";
+
 export const createUser = async (req, res) => {
-  console.log("req.body:", req.body);
   const user = await User.create({
     name: req.body.name,
   });
+  // delete old cache data
+  const keys = await redis.keys(`${root}:users:*`);
+  if (keys.length > 0) {
+    redis.del(...keys);
+  }
+
   res.json(user);
 };
 
@@ -13,30 +21,116 @@ export const getAll = async (req, res) => {
   const limit = Number(req.query.limit) || 10;
 
   const skip = (page - 1) * limit;
-  const users = await User.find().skip(skip).limit(limit).sort({createdAt: -1});
 
-  const total = await User.countDocuments();
+  // redis cache key
+  const cacheKey = `${root}:users:page:${page}:limit:${limit}`;
+  // redis lock key
+  const lockKey = `${root}:lock:${cacheKey}`;
 
-  res.json({
-    users,
-    hasMore: page * limit < total,
-    totalPages: Math.ceil(total / limit),
-    currentPage: page,
+  const cachedUsers = await redis.get(cacheKey);
+
+  if (cachedUsers) {
+    // console.log("Cache hitted");
+    return res.json(cachedUsers);
+  }
+
+  // console.log("Cache missed");
+
+  // lock aquire
+  const lock = await redis.set(lockKey, "locked", {
+    nx: true,
+    ex: 5,
   });
+
+  if (!lock) {
+    // console.log("lock not aquired");
+
+    // wait
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    // retry cache entry
+    const retryCache = await redis.get(cacheKey);
+
+    if (retryCache) {
+      return res.json(retryCache);
+    }
+  }
+
+  // real db fetch
+  try {
+    const users = await User.find()
+      .skip(skip)
+      .limit(limit)
+      .sort({ createdAt: -1 });
+
+    const total = await User.countDocuments();
+
+    const responseData = {
+      users: users,
+      hasMore: page * limit < total,
+      totalPages: Math.ceil(total / limit),
+      currentPage: page,
+    };
+
+    await redis.set(cacheKey, responseData, {
+      ex: 120,
+    });
+
+    // return res from db
+    return res.json(responseData);
+  } catch (error) {
+    console.log("Error in getAll", error.message);
+    return res.json({ message: "Server error" });
+  } finally {
+    // Release lock
+    await redis.del(lockKey);
+    // console.log("lock released")
+  }
 };
 
 export const toggleActive = async (req, res) => {
-  const user = await User.findById(req.params.id);
+  try {
+    const userId = req.params.id;
 
-  if (!user) {
-    return res.status(404).json({
-      message: "User not found",
+    const lockKey = `${root}:lock:user:${req.params.id}`;
+
+    const lock = await redis.set(lockKey, "locked", {
+      nx: true,
+      ex: 5,
     });
+
+    if (!lock) {
+      return res.status(429).json({
+        message: "User is already being updated",
+      });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    user.active = !user.active;
+
+    await user.save();
+
+    const keys = await redis.keys(`${root}:users:*`);
+    if (keys.length > 0) {
+      await redis.del(...keys);
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.log("Error in toggleActive:", error.message);
+
+    return res.status(500).json({
+      message: "Server error",
+    });
+  } finally {
+    // release lock
+    // await redis.del(`${root}:lock:user:${req.params.id}`);
   }
-
-  user.active = !user.active;
-
-  await user.save();
-
-  res.json(user);
+  
 };
